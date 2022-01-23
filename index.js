88eeeeab4169d23d637c23a6b3b6d9fedb22474a
Demian Fueglistaler <info@webcrunch.ch>
2022-01-23 03:53:17 +0100

    ccomment out db stuff

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const app = express();
 
 
 
-const connection = mysql.createPool({
-        connectionLimit: 5,
-    host: 'localhost',
-    port: 8889,
-    user: 'root',
-    password: 'root',
-    database: 'crawl_jobs'
-});
+// const connection = mysql.createPool({
+//         connectionLimit: 5,
+//     host: 'localhost',
+//     port: 8889,
+//     user: 'root',
+//     password: 'root',
+//     database: 'crawl_jobs'
+// });
 
 
 
@@ -51,56 +51,56 @@ app.get ('/company/:id*', (req,res) => {
                 res.json(err) 
         )
 }) 
-app.get ('/jobs/:pages*', (req,res) => {
+// app.get ('/jobs/:pages*', (req,res) => {
         
-       const page = req.params['pages']
-        const results_per_page = 33;
-        const page_first_result = page * results_per_page;  
+//        const page = req.params['pages']
+//         const results_per_page = 33;
+//         const page_first_result = page * results_per_page;  
 
         
-        // Get total number of pages
-        const query = "SELECT * FROM jobs_ch";  
-        const result = connection.query(connection, query);  
+//         // Get total number of pages
+//         const query = "SELECT * FROM jobs_ch";  
+//         const result = connection.query(connection, query);  
         
 
-                connection.getConnection(function(err, connection) {
+//                 connection.getConnection(function(err, connection) {
 
-                        connection.query("SELECT * FROM jobs_ch", function (err, result, fields) {
-                                const number_of_result = result.length;
-                                const number_of_page = Math.ceil(number_of_result / results_per_page) - 1; 
-                                console.log(number_of_page) 
+//                         connection.query("SELECT * FROM jobs_ch", function (err, result, fields) {
+//                                 const number_of_result = result.length;
+//                                 const number_of_page = Math.ceil(number_of_result / results_per_page) - 1; 
+//                                 console.log(number_of_page) 
 
-                        });
-                        connection.release()
+//                         });
+//                         connection.release()
                        
-                });
+//                 });
                 
-        const final_query = "SELECT * FROM jobs_ch LIMIT " + page_first_result + ", " + results_per_page ;    
+//         const final_query = "SELECT * FROM jobs_ch LIMIT " + page_first_result + ", " + results_per_page ;    
 
 
 
 
 
 
-        connection.getConnection(function(err, connection) {
+//         connection.getConnection(function(err, connection) {
 
-                        connection.query(final_query, function (err, result, fields) {
-                                if (result.length > 0) {
-                                        console.log(result)
-                                        res.json(result)
-                                } else {
-                                        res.json('Out of Range')
-                                }
+//                         connection.query(final_query, function (err, result, fields) {
+//                                 if (result.length > 0) {
+//                                         console.log(result)
+//                                         res.json(result)
+//                                 } else {
+//                                         res.json('Out of Range')
+//                                 }
 
-                        });
-                        connection.release()
+//                         });
+//                         connection.release()
                        
-                });
+//                 });
                 
         
         
         
-})
+// })
 
 
 
